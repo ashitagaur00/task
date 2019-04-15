e93634c4e1e78ac64ce4858b9326547cb191e96e
Ashita Gaur <microsmall87@gmail.com>
2019-04-15 22:57:12 +0530

    first change

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+@Component({
+  selector: 'app-root''mat-slide-toggle',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'task';
+}
